97dcbe872a07c4062f5bf9d74267f518e049cf45
sofoinasgd <zephaniahsofa@gmail.com>
2024-05-20 11:33:20 +0000

    task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
-# install flask using pip
-package { 'flask':
-ensure   => '2.1.0',
-provider => 'pip3',
+#!/usr/bin/pup
+# install flask using puppet
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
