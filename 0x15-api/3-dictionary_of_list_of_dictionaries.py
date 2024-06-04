0c3ebb9a5f225d831e4dcb4db905307c859c46c8
sofoinasgd <zephaniahsofa@gmail.com>
2024-06-04 11:13:58 +0000

    task 3

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,18 +1,45 @@
 #!/usr/bin/python3
-"""Exports to-do list information of all employees to JSON format."""
+"""3. Dictionary of list of dictionaries
+exports employee tasks data for all employees"""
+
+
 import json
 import requests
 
 if __name__ == "__main__":
+    """ export data in the JSON format for every employee
+    export format:
+    { "USER_ID": [{"task": "TASK_TITLE", "completed": TASK_COMPLETED_STATUS,
+    "username": "USERNAME"}, {"task": "TASK_TITLE",
+    "completed": TASK_COMPLETED_STATUS, "username": "USERNAME"}, ... ]}
+    """
     url = "https://jsonplaceholder.typicode.com/"
-    users = requests.get(url + "users").json()
 
-    with open("todo_all_employees.json", "w") as jsonfile:
-        json.dump({
-            u.get("id"): [{
-                "task": t.get("title"),
-                "completed": t.get("completed"),
-                "username": u.get("username")
-            } for t in requests.get(url + "todos",
-                                    params={"userId": u.get("id")}).json()]
-            for u in users}, jsonfile)
+    # get todos json for tasks list
+    task_list = requests.get(url + "todos").json()
+
+    user_dictionary = {}
+    user_tasks = []
+    task_dict = {}
+    tasks_total = 0
+
+    for task in task_list:
+        user_id = task["userId"]
+        # if new user_id create dictionary entry
+        if task['userId'] not in user_dictionary.keys():
+            user_dictionary[user_id] = []
+
+        task_dict = {}
+        # get username
+        user = requests.get(url + "users/" + str(user_id)).json()["username"]
+        task_dict["task"] = task['title']
+        task_dict["completed"] = task['completed']
+        task_dict["username"] = user
+
+        # add dict to the right list
+        task_list = user_dictionary[user_id]
+        task_list.append(task_dict)
+
+    # write json
+    with open("todo_all_employees.json", "w") as file:
+        json.dump(user_dictionary, file)
