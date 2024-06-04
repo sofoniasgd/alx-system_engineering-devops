23b9214c33ebcdd5af78de1f3532b6a942414130
sofoinasgd <zephaniahsofa@gmail.com>
2024-06-04 10:15:01 +0000

    task 2

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -3,6 +3,7 @@
 exports employee tasks data"""
 
 
+import json
 import requests
 import sys
 
@@ -16,13 +17,26 @@ if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
     user_id = sys.argv[1]
 
-    task_list = requests.get(url + "todos/" + user_id).json()
+    task_list = requests.get(url + "todos").json()
 
-    print(type(task_list))
     # get todos json for tasks list
-    tasks_done = []
+    user_dictionary = {}
+    user_tasks = []
+    task_dict = {}
     tasks_total = 0
-    todo = requests.get(url + "todos").json()
+    for task in task_list:
+        if task['userId'] == int(user_id):
+            # get username
+            user = requests.get(url + "users/" + user_id).json()["username"]
+            task_dict["task"] = task['title']
+            task_dict["completed"] = task['completed']
+            task_dict["username"] = user
 
-    # write CSV
-    #with open(e_id + ".csv", "w") as file:
+            # add dict to list
+            user_tasks.append(task_dict)
+    # final json dict
+    user_dictionary[user_id] = user_tasks
+
+    # write json
+    with open(user_id + ".json", "w") as file:
+        json.dump(user_dictionary, file)
