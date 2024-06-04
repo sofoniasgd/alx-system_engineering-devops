7b6c20890c039d1f608950f45b9dccc5c4e9bf8e
sofoinasgd <zephaniahsofa@gmail.com>
2024-06-04 09:38:51 +0000

    task 2

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,19 +1,28 @@
 #!/usr/bin/python3
-"""Exports to-do list information for a given employee ID to JSON format."""
-import json
+"""2. Export to JSON
+exports employee tasks data"""
+
+
 import requests
 import sys
 
 if __name__ == "__main__":
-    user_id = sys.argv[1]
+    """ export data in the JSON format
+    export format:
+    { "USER_ID": [{"task": "TASK_TITLE", "completed": TASK_COMPLETED_STATUS,
+    "username": "USERNAME"}, {"task": "TASK_TITLE",
+    "completed": TASK_COMPLETED_STATUS, "username": "USERNAME"}, ... ]}
+    """
     url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(user_id)).json()
-    username = user.get("username")
-    todos = requests.get(url + "todos", params={"userId": user_id}).json()
+    user_id = sys.argv[1]
 
-    with open("{}.json".format(user_id), "w") as jsonfile:
-        json.dump({user_id: [{
-                "task": t.get("title"),
-                "completed": t.get("completed"),
-                "username": username
-            } for t in todos]}, jsonfile)
+    task_list = requests.get(url + "todos/" + user_id).json()
+
+    print(type(task_list))
+    # get todos json for tasks list
+    tasks_done = []
+    tasks_total = 0
+    todo = requests.get(url + "todos").json()
+
+    # write CSV
+    #with open(e_id + ".csv", "w") as file:
