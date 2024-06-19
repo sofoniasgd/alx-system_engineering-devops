7b000cd0a998a6bf44abf32babfcea4bf23d5d18
sofoniasgd <zephaniahsofa@gmail.com>
2024-06-19 08:57:31 +0000

    task 0 update

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,6 +9,8 @@ def number_of_subscribers(subreddit):
     (not active users, total subscribers)
     from the Reddit API
     """
+    if subreddit is None or type(subreddit) is not str:
+        return 0
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     response = requests.get(url, headers={'User_Agent': 'sofi_vm'},
                             allow_redirects=False)
