1d62808e815400cfe3f05bf97a0ae2152b31dd7f
sofoniasgd <zephaniahsofa@gmail.com>
2024-06-19 09:33:09 +0000

    updated task 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -8,13 +8,17 @@ def top_ten(subreddit):
     """queries the first 10 'hot' posts
     from the Reddit API
     """
+    if subreddit is None or type(subreddit) is not str:
+        print("None")
+        return
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    response = requests.get(url, headers={'User_Agent': 'sofi_vm'},
-                            params={'limit': 10},
+    response = requests.get(url, headers={"User_Agent": "sofi_vm"},
+                            params={"limit": 10},
                             allow_redirects=False)
     if response.status_code == 200:
-        post_list = response.json()['data']['children']
+        post_list = response.json()["data"]["children"]
         for post in post_list:
-            print(post['data']['title'])
+            print(post["data"]["title"])
     else:
         print("None")
+        return
