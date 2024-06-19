7652f9d9845c596b9372de3b09fd237240f63a1c
sofoniasgd <zephaniahsofa@gmail.com>
2024-06-19 10:15:19 +0000

    updated task 3

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -10,27 +10,21 @@ def recurse(subreddit, hot_list=[], after=""):
     using !!!RECURSION!!!
     """
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    if after != "":
-        response = requests.get(url, headers={'User_Agent': 'sofi_vm'},
-                                params={"after": after, "limit": 1},
-                                allow_redirects=False)
-    else:
-        response = requests.get(url, headers={'User_Agent': 'sofi_vm'},
-                                 params={"limit": 1},
-                                 allow_redirects=False)
-    if response.status_code == 200:
+    response = requests.get(url, headers={"User_Agent": "sofi_vm"},
+                            params={"after": after, "limit": 100},
+                            allow_redirects=False)
+    if response.status_code == 404:
+        return None
 
-        # parse and add titles to list
-        post_list = response.json()['data']['children']
-        # print(response.status_code)
-        for post in post_list:
-            hot_list.append(post['data']['title'])
-        # check if there are no more items to retrieve
-        after = response.json()['data']['after']
-        if after is None:
-            return hot_list
-        # call function with after parameter
+    # parse and add titles to list
+    post_list = response.json()["data"]["children"]
+    for post in post_list:
+        hot_list.append(post.get("data").get("title"))
+
+    # check if there are no more items to retrieve
+    after = response.json().get("data").get("after")
+    # call function with after parameter
+    if after is not None:
         recurse(subreddit, hot_list, after)
-        return hot_list
-    else:
-        exit()
+
+    return hot_list
